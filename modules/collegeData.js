e6c3f2f1919c377fa389c853c523c02493ba3866
TUAN DUONG HUYNH <tuanduong.huynh7698@gmail.com>
2025-02-04 15:12:14 -0500

    preparation

---
+++ modules/collegeData.js
@@ -0,0 +1,64 @@
+class Data {
+    constructor(students, courses) {
+        this.students = students;
+        this.courses = courses;
+    }
+}
+
+let dataCollection = null;
+
+const { constants } = require("crypto");
+const fs = require("fs/promises");
+
+async function initialize() {
+    try {
+        const studentsData = await fs.readFile("./data/students.json", "utf8");
+        const coursesData = await fs.readFile("./data/courses.json", "utf8");
+
+        const students = JSON.parse(studentsData);
+        const courses = JSON.parse(coursesData);
+
+        dataCollection = new Data(students, courses);
+        return Promise.resolve(); // Resolve with the number of students
+    } catch (err) {
+        return Promise.reject(`${err.message}`);
+    }
+}
+
+function getAllStudents() {
+    return new Promise((resolve, reject) => {
+        if (dataCollection && dataCollection.students.length > 0) {
+            resolve(dataCollection.students);
+        } else {
+            reject("No students found.");
+        }
+    });
+}
+
+function getTAs() {
+    return new Promise((resolve, reject) => {
+        const studentTAs = [];
+        for(let i = 0; i < dataCollection.students.length; i++) {
+            if(dataCollection.students[i].TA){
+                studentTAs.push(dataCollection.students[i])
+            }
+        }
+        if (studentTAs.length > 0){
+            resolve(studentTAs);
+        } else {
+            reject(`no results returned`)
+        }
+    }); 
+}
+
+function getCourses() {
+    return new Promise((resolve, reject) => {
+        if (dataCollection && dataCollection.courses.length > 0) {
+            resolve(dataCollection.courses)
+        } else {
+            reject("No courses found")
+        }
+    })
+}
+
+module.exports = {initialize,getAllStudents,getTAs,getCourses}
