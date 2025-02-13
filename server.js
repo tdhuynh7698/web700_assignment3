931d07168ec1e8303e3325bbdb51649db04bf65c
TUAN DUONG HUYNH <tuanduong.huynh7698@gmail.com>
2025-02-13 17:05:54 -0500

    /student

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
-var college = require("./collegeData.js")
+var college = require("./modules/collegeData")
 // setup a 'route' to listen on the default url path
 app.get("/students", (req, res) => {
-    college.getAllStudents()
-    .then(students => {
-        res.json(students);
-    })
-    .catch(err => {
-        res.status(500).json({ message: "No results"});
-    });
-    res.send("Hello World!");
+    if(req.query.course){
+        college.getStudentsByCourse(req.query.course)
+        .then(students => {
+            res.json(students);
+        })
+        .catch(err => {
+            res.status(500).json({message: "No results"});
+        });
+    } else{
+        college.getAllStudents()
+        .then(students => {
+            res.json(students);
+        })
+        .catch(err => {
+            res.status(500).json({ message: "No results"});
+        });
+    }
 });
 
 // setup http server to listen on HTTP_PORT
